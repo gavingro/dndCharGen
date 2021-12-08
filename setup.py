a1e1973c086c265631dfb6d033c833472bd43d83
kradford7 <kevin.radford117@gmail.com>
2021-12-08 14:22:01 -0800

    ran setup and uploaded to pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(exclude=['test*']),
     license='MIT',
     description='A python package to generate a D&D character and populate a character sheet.',
-    install_requires=["pdfrw = 0.4"],
+    install_requires=["pdfrw >= 0.4"],
     url='https://github.com/gavingro/DATA533proj3_dndCharGen.git',
     author='gavingro, kradford7',
     author_email='grochowskigavin@gmail.com, kevin.radford117@gmail.com'
