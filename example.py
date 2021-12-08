d39a4d23ab881feba16bbd7649539bfa6a8c6315
Gavin Grochowski <grochowskigavin@gmail.com>
2021-12-08 10:30:52 -0800

    Wrap example.py in main function

--- example.py
+++ example.py
@@ -7,17 +7,23 @@ from generator.gameclass import GameClass
 from generator.race import Race
 from generator.stats import Stats
 
-# Generate and display a random character
-mychar = Character()
-ci_display(mychar)
-print('-----------------------------------------------------------')
-cs_display(mychar)
 
-# Generate a custom character
-base_stats = Stats(STR = 15, DEX = 3, CON = 20, INT = 10, WIS = 8, CHA = 7)
-game_class = GameClass('Barbarian', auto_generate=True)
-race = Race('Dwarf', auto_generate=True)
-background = Background('Acolyte', auto_generate=True)
+def main():
+    # Generate and display a random character
+    mychar = Character()
+    ci_display(mychar)
+    print("-----------------------------------------------------------")
+    cs_display(mychar)
 
-mychar = Character(base_stats, game_class, race, background, auto_generate=False)
-save_to_pdf(mychar, flatten=True)
+    # Generate a custom character
+    base_stats = Stats(STR=15, DEX=3, CON=20, INT=10, WIS=8, CHA=7)
+    game_class = GameClass("Barbarian", auto_generate=True)
+    race = Race("Dwarf", auto_generate=True)
+    background = Background("Acolyte", auto_generate=True)
+
+    mychar = Character(base_stats, game_class, race, background, auto_generate=False)
+    save_to_pdf(mychar, flatten=True)
+
+
+if __name__ == "__main__":
+    main()
