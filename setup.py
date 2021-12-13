b003ee4672222554dcd5fb41ef00dbeeae7496bd
kradford7 <kevin.radford117@gmail.com>
2021-12-12 17:38:05 -0800

    incremented version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dndcharactergenerator",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(exclude=["test*"]),
     license="MIT",
     description="A python package to generate a D&D character and populate a character sheet.",
